d952e61f9e22623619d0c149f2b6a1890e6a813e
Domen Oslaj <doslaj@gmail.com>
2020-10-02 22:23:16 +0200

    added random funktion + 3 elif

--- game.py
+++ game.py
@@ -1,8 +1,20 @@
-some_num = 32 #hard-code
+import random
 
-guess = int(input("Please guess the secret number: "))
+secret = random.randint(1, 10)
+guess = 0
 
-if guess == some_num:
-    print("Congratulations!")
-else:
-    print("Sorry, try one more time.")
+# endless loop
+while True:
+    guess = int(input("Please guess the secret number between 1 and 10: "))
+
+    if guess == secret:
+        print("Congratulations! Secret number is number " + str(secret) + ".")
+        break  # break the loop
+    elif guess > 10:
+        print("Choose number between 1 and 10!")
+    elif guess > secret:
+        print("Your guess is not correct. Try something smaller.")
+    elif guess < secret:
+        print("Your guess is not correct. Try something bigger.")
+    else:
+        print("Please enter a number!")
