a765e9ffdb4a53ff9ec36a15d5e0e128673682f1
Domen Oslaj <doslaj@gmail.com>
2020-10-06 21:10:21 +0200

    added name, secret to the dict)

--- game.py
+++ game.py
@@ -10,12 +10,10 @@ current_time = time.strftime("%A " "%d-" "%m-" "%Y  " "%H:" "%M:" "%S")
 
 with open("score_list.txt", "r") as score_file:
     score_list = json.loads(score_file.read())
-    #print(score_list)
 
 for score_dict in score_list:
-    #print(score_dict)
-    print("Attempts: " + str(score_dict.get("attempts")) + " | " + "Date: " + score_dict.get("date"))
-
+    print("Attempts: " + str(score_dict.get("attempts")) + " | " + "Date: " + score_dict.get("date") + " | " + "Name: "
+          + score_dict.get("name") + " | " + "Secret number: " + str(score_dict.get("secret")))
 
 # endless loop
 while True:
@@ -26,7 +24,9 @@ while True:
         print("Congratulations! Secret number is number " + str(secret) + ".")
         print("Attempts needed: {0}" .format(attempts))    #format method to join 2 strings
 
-        score_list.append({"attempts": attempts, "date": current_time})
+        name = input("I want to save you name, score and secret number. Could you tell me you name? ")
+
+        score_list.append({"attempts": attempts, "date": current_time, "name": name, "secret": secret})
 
         with open("score_list.txt", "w") as score_file:
             score_file.write(json.dumps(score_list))
