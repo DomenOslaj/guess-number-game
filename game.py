1f5d6d07974599bca7688404abfc91708a7aaebe
Domen Oslaj <doslaj@gmail.com>
2020-10-06 23:09:02 +0200

    added wrong_guesses list

--- game.py
+++ game.py
@@ -2,6 +2,8 @@ import random
 import json
 import datetime
 
+name = input("I want to save you name, score and secret number. Could you tell me you name? ")
+
 secret = random.randint(1, 10)
 attempts = 0
 
@@ -12,21 +14,29 @@ with open("score_list.txt", "r") as score_file:
     score_list = json.loads(score_file.read())
 
 for score_dict in score_list:
-    print("Attempts: " + str(score_dict.get("attempts")) + " | " + "Date: " + score_dict.get("date") + " | " + "Name: "
-          + score_dict.get("name") + " | " + "Secret number: " + str(score_dict.get("secret")))
+    text = "Attempts: {0} | Date and hour: {1} | Player: {2} | Secret number: {3} | Wrong guesses: {4}".format(str(
+                                                                                            score_dict.get("attempts")),
+                                                                                            score_dict.get("date"),
+                                                                                            score_dict.get("name"),
+                                                                                            score_dict.get("secret"),
+                                                                                            score_dict.get(
+                                                                                                "wrong guesses"))
+    print(text)
+
+wrong_guesses = []
 
 # endless loop
 while True:
     guess = int(input("Please guess the secret number between 1 and 10: "))
-    attempts += 1    #+= adds a number to a variable, changing the variable itself in the process
+
+    attempts += 1    # += adds a number to a variable, changing the variable itself in the process
 
     if guess == secret:
         print("Congratulations! Secret number is number " + str(secret) + ".")
-        print("Attempts needed: {0}" .format(attempts))    #format method to join 2 strings
+        print("Attempts needed: {0}" .format(attempts))    # format method to join 2 strings
 
-        name = input("I want to save you name, score and secret number. Could you tell me you name? ")
-
-        score_list.append({"attempts": attempts, "date": current_time, "name": name, "secret": secret})
+        score_list.append({"attempts": attempts, "date": current_time, "name": name, "secret": secret,
+                           "wrong guesses": wrong_guesses})
 
         with open("score_list.txt", "w") as score_file:
             score_file.write(json.dumps(score_list))
@@ -40,3 +50,5 @@ while True:
 
     elif guess < secret:
         print("Your guess is not correct. Try something bigger.")
+
+    wrong_guesses.append(guess)
